843dfbb4912cb79c4c433873aea3fc7927593ade
Dimitris Kostaras <dimitris.k@inspired-mobile.com>
2017-02-19 21:02:07 +0200

    make products dynamic, add gitignore

--- WebShop/Scripts/controllers/productsController.js
+++ WebShop/Scripts/controllers/productsController.js
@@ -3,9 +3,9 @@ app.controller('productsController', [
     '$scope',
     function productsController($scope) {
         $scope.items = [
-      { name: 'Bed', price: '699', oldprice: '760' },
-      { name: 'Nightstand', price: '399', oldprice: '450' },
-      { name: 'Hammock', price: '599', oldprice: '640' }
+      { name: 'Bed', price: '699', oldprice: '760', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien risus, blandit at fringilla ac, varius sed dolor. Donec augue lacus, vulputate sed consectetur facilisis, interdum pharetra ligula. Nulla suscipit erat nibh, ut porttitor nisl dapibus eu.', image: 'product1.jpg' },
+      { name: 'Nightstand', price: '399', oldprice: '450', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien risus, blandit at fringilla ac, varius sed dolor. Donec augue lacus, vulputate sed consectetur facilisis, interdum pharetra ligula. Nulla suscipit erat nibh, ut porttitor nisl dapibus eu.', image: 'product2.jpg' },
+      { name: 'Hammock', price: '599', oldprice: '640', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien risus, blandit at fringilla ac, varius sed dolor. Donec augue lacus, vulputate sed consectetur facilisis, interdum pharetra ligula. Nulla suscipit erat nibh, ut porttitor nisl dapibus eu.', image: 'product3.jpg' }
         ];
     }
 ]);
